377dd19b8a9a86ad66804e0e62dea8e9505736ba
Freddy Li <tobeacookie@hotmail.com>
2021-05-26 21:17:06 -0400

    refactor ownership transfer

--- contracts/DSAuth.sol
+++ contracts/DSAuth.sol
@@ -6,22 +6,36 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract DSAuth is Ownable, AccessControl {
     bytes32 public constant MINT_BURN_ROLE = keccak256("MINT_BURN_ROLE");
+    address private _pendingOwner;
 
-    // setOwner transfers the SPWN token contract ownership to another address
+    function setPendingOwner(address pendingOwner) public onlyOwner {
+        require(pendingOwner != owner(), "Pending owner and current owner need to be different");
+        require(pendingOwner != address(0), "Pending owner can not be zero address");
+
+        _pendingOwner = pendingOwner;
+    }
+
+    function getPendingOwner() public view onlyOwner returns (address) {
+        return _pendingOwner;
+    }
+
+    // acceptOwner allows the current owner to accept and finalize the ownership transfer of the SPWN token contract
     // along with grant new owner MINT_BURN_ROLE role and remove MINT_BURN_ROLE from old owner
     // note: call transferOwnerShip will only change ownership without other roles
-    function setOwner(address newOwner) public onlyOwner {
-        require(newOwner != owner(), "New owner and current owner need to be different");
+    function acceptOwner() public onlyOwner {
+        require(_pendingOwner != address(0), "Please set pending owner first");
 
         address oldOwner = owner();
 
-        transferOwnership(newOwner);
+        transferOwnership(_pendingOwner);
 
-        _grantAccess(MINT_BURN_ROLE, newOwner);
+        _grantAccess(MINT_BURN_ROLE, _pendingOwner);
         _revokeAccess(MINT_BURN_ROLE, oldOwner);
 
-        _setupRole(DEFAULT_ADMIN_ROLE, newOwner);
+        _setupRole(DEFAULT_ADMIN_ROLE, _pendingOwner);
         _revokeAccess(DEFAULT_ADMIN_ROLE, oldOwner);
+
+        _pendingOwner = address(0);
     }
 
     // setAuthority performs the same action as grantMintBurnRole
